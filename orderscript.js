173e9cad885a314921099f6c319a157244399416
Maddiesa <joma24rw@student.ju.se>
2026-02-25 15:04:44 +0100

    MoreItemsAddedToTabs

--- orderscript.js
+++ orderscript.js
@@ -1,3 +1,4 @@
+let cart = [];
 console.log("Order script loaded");
 document.addEventListener("DOMContentLoaded", () => {
   const navLinks = document.querySelectorAll(".order-nav");
@@ -7,14 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
 
-      const targetId = link.getAttribute("data-tab");
+      const targetId = link.dataset.tab;
       const targetTab = document.getElementById(targetId);
 
       if (!targetTab) return;
 
       tabs.forEach((tab) => tab.classList.remove("active"));
-      navLinks.forEach((l) => l.classList.remove("active"));
-
+      navLinks.forEach((nav) => nav.classList.remove("active"));
+      // activate clicked tab //
       targetTab.classList.add("active");
       link.classList.add("active");
     });
